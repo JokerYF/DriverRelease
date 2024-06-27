5f597b54e4155c818f27cd4bd78869b2f7e9aad8
御风 <eternallyyf@163.com>
2024-06-27 12:09:17 +0800

    feat: 新增ChromeDriverLatestPatchVersion.json文件

--- update_chrome_driver_release.py
+++ update_chrome_driver_release.py
@@ -5,7 +5,9 @@
 from common import RequestBase, JsonParser
 
 url = 'https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json'
+url2 = 'https://googlechromelabs.github.io/chrome-for-testing/latest-patch-versions-per-build.json'
 json_parser = JsonParser('./ChromeDriverLastVersion.json')
+json_parser2 = JsonParser('./ChromeDriverLatestPatchVersion.json')
 
 data = RequestBase(url).get
 
@@ -13,3 +15,9 @@ if json_parser.get('timestamp', '') != data['timestamp']:
     json_parser.set(data)
     print(data)
     print('Update ChromeDriverLastVersion.json Succeed!')
+
+data2 = RequestBase(url).get
+if json_parser2.get('timestamp', '') != data2['timestamp']:
+    json_parser2.set(data2)
+    print(data2)
+    print('Update ChromeDriverLatestPatchVersion.json Succeed!')
